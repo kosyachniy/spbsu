7e2bd84d987b8f13d198efb88621a3a46105227e
Poloz Alexey <polozhev@mail.ru>
2017-08-23 15:42:13 +0300

    Questions to users

--- -/create.py
+++ -/create.py
@@ -5,5 +5,5 @@ sys.path.append('../')
 from spbsu.func import *
 
 with db:
-	db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, faculty text, direction int, program int, year int, groups int)")
+	db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, faculty text, direction int, program int, year int, groups int, num int)")
 	db.execute("CREATE TABLE timetable (id text PRIMARY KEY, num int)")
